1400e91cca77361bfd52423c1bf8f12b811d20fb
Lillam <liam.taylor@outlook.com>
2024-09-01 22:13:46 +0100

    chore: cleaning up make journal loan request from importing auth to use handle request internal method for user.

--- app/Http/Requests/MakeJournalLoanRequest.php
+++ app/Http/Requests/MakeJournalLoanRequest.php
@@ -4,14 +4,13 @@ namespace App\Http\Requests;
 
 use Carbon\Carbon;
 use App\Models\Journal\JournalLoan;
-use Illuminate\Support\Facades\Auth;
 
 class MakeJournalLoanRequest extends HandleRequest
 {
     public function handle(): bool
     {
         JournalLoan::query()->create([
-            'user_id'       => Auth::id(),
+            'user_id'       => $this->getUserId(),
             'amount'        => $this->request->get('amount'),
             'interest'      => $this->request->get('interest'),
             'reference'     => $this->request->get('reference'),
